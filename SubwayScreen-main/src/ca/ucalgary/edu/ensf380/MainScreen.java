9d152893a85789bddd1a96c662cc69aa473d8294
Fatima Asif <fatima.asif@ucalgary.ca>
2024-07-30 14:59:53 -0600

    updates to mainScreen

--- SubwayScreen-main/src/ca/ucalgary/edu/ensf380/MainScreen.java
+++ SubwayScreen-main/src/ca/ucalgary/edu/ensf380/MainScreen.java
@@ -1,38 +1,44 @@
 package ca.ucalgary.edu.ensf380;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import javax.swing.*;
 
-public class MainScreen extends Thread {
+import javax.swing.*;
+import javax.swing.JLabel;
+
+public class MainScreen extends Thread{
     private static JLabel tempLabel;
     private static JLabel windLabel;
     private static JLabel rainLabel;
     private static JLabel timeLabel;
-    private static JPanel adPanel;
+   
+    private static JPanel newsPanel;
 
     public MainScreen() {
         start(); // Starts the thread upon class initialization
     }
 
+	//starts a new thread
+    //basically does multiple things at once such as continues to display the time over and over
     public void run() {
-        while (true) {
-            displayTime();
-            try {
-                Thread.sleep(1000); // Update time every second
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+    	while(true) {
+    		displayTime();
+    	}
     }
-
+    
     public static void displayTime() {
-        LocalDateTime localDate = LocalDateTime.now();
+    	LocalDateTime localDate = LocalDateTime.now();
+        
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a");
         String dtfTime = dtf.format(localDate);
+        
+        //update this variable with the new text which is the time 
         timeLabel.setText("Time: " + dtfTime);
     }
+
+  
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -44,6 +50,11 @@ public class MainScreen extends Thread {
         // Create instances of other classes using the provided constructors
         WeatherParser weatherParser = new WeatherParser(cityName);
         WeatherService weatherService = new WeatherService(weatherParser);
+
+        // Create an instance of NewsProvider
+        String endpoint = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2fba803407f040ccb4a075a558ea4a24";
+        NewsProvider newsProvider = new NewsProvider(endpoint);
+
 
         // Initialize GUI components
         JFrame frame = new JFrame("Subway Screen");
@@ -61,9 +72,9 @@ public class MainScreen extends Thread {
         windLabel = new JLabel("Wind: " + wind);
         rainLabel = new JLabel("Rain: " + rain);
         timeLabel = new JLabel("Time: ");
-        displayTime(); 
-
-        // Add these components to a weather panel
+        displayTime(); //UNNECESSARY BUT KEEP UNTIL THE VERY END
+        
+        //ADD THESE COMPONENTS TO A WEATHER PANEL
         JPanel weatherPanel = new JPanel();
         weatherPanel.setLayout(new BoxLayout(weatherPanel, BoxLayout.Y_AXIS));
         weatherPanel.add(tempLabel);
@@ -73,16 +84,19 @@ public class MainScreen extends Thread {
 
         frame.add(weatherPanel);
 
-        // Advertisement section
-        Advertisement advertisement = new Advertisement();
-        advertisement.display();
-        adPanel = advertisement.getAdPanel();
-        frame.add(adPanel);
+        // News section
+        newsProvider.display(); // make sure Info for the display method is there
+        newsPanel = newsProvider.getNewsPanel(); // get the newspanel from NewsProvider
+        newsPanel.setBackground(Color.ORANGE);
+        frame.add(newsPanel); //add to subwayScreen 
 
         frame.setVisible(true);
-
-        // Instantiate MainScreen
+        
+        // Instantiate MainScreen with all the objects
         MainScreen mainScreen = new MainScreen();
     }
+
+
 }
+
 
